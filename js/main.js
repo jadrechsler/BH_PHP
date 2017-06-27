9512cd0f10f634e11c3786d03280543e1fe82e36
jtsalva <jtsalva@users.noreply.github.com>
2017-06-27 15:18:12 +0100

    Main Layout

---
+++ js/main.js
@@ -0,0 +1,9 @@
+const overlay = document.getElementById("overlay");
+
+function overlay_show(id, name) {
+	overlay.style.visibility = "visible";
+}
+
+function overlay_hide() {
+	overlay.style.visibility = "hidden";
+}
