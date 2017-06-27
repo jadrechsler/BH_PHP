24142670307ff18e9affffc9826ebe6cf4579ab8
jtsalva <jtsalva@users.noreply.github.com>
2017-06-27 17:35:29 +0100

    Check in/out Options

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
 	<div id="overlay">
 		<div class="container-fluid options">
 			<div class="col-md-4">
-				<div id="check-in-out" class="option">
+				<div id="check-in-out" class="option" onclick="check_in_out_show()">
 					<p>Check in/out</p>
 				</div>
 			</div>
@@ -24,9 +24,31 @@
 				</div>
 			</div>
 			<div class="col-md-4">
-				<div id="exit"><span onclick="overlay_hide()">&#x2573</span></div>
+				<div class="exit"><span onclick="overlay_hide()">&#x2573</span></div>
 				<div id="change-pin" class="option">
 					<p>Change pin</p>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div id="overlay-check-in-out">
+		<div class="container-fluid options">
+			<div class="col-md-4">
+				<div class="back"><span onclick="check_in_out_hide()">&#x261A</span></div>
+				<div id="check-in-out" class="option" onclick="check_in_out_show()">
+					<p>Check in/out</p>
+				</div>
+			</div>
+			<div class="col-md-4">
+				<div id="check-in" class="option">		
+					<p>Check in</p>
+				</div>
+			</div>
+			<div class="col-md-4">
+				<div class="exit"><span onclick="overlay_hide()">&#x2573</span></div>
+				<div id="check-out" class="option">
+					<p>Check out</p>
 				</div>
 			</div>
 		</div>
@@ -43,7 +65,7 @@
 			<div class="col-md-4"></div>
 		</div>
 		<div id="main" class="container-fluid">
-			<?php echo makeRowSet($children); ?>
+			<?php echo makeRowSet($children) ?>
 		</div>
 	</div>
 	<script src="js/main.js"></script>
--- js/main.js
+++ js/main.js
@@ -1,10 +1,34 @@
 const overlay = document.getElementById("overlay");
+const check_in_out = document.getElementById("overlay-check-in-out")
+
+// Passed in elements made visible
+function show(elements) {
+	Array.prototype.slice.call(arguments).forEach(function(element, index) {
+		element.style.visibility = "visible";
+	});
+}
+
+// Passed in elements made hidden
+function hide(elements) {
+	Array.prototype.slice.call(arguments).forEach(function(element, index) {
+		element.style.visibility = "hidden";
+	});
+}
+
+function check_in_out_show() {
+	show(check_in_out);
+}
+
+function check_in_out_hide() {
+	hide(check_in_out);
+}
 
 function overlay_show(id, name) {
 	document.getElementById("name").innerHTML = name;
-	overlay.style.visibility = "visible";
+	show(overlay);
 }
 
 function overlay_hide() {
-	overlay.style.visibility = "hidden";
+	document.getElementById("name").innerHTML = "";
+	hide(overlay, check_in_out);
 }
